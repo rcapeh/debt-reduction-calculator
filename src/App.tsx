9f98fed32e78fecaac86dcfd6ff6e00b4f59a796
Ryan Capehart <ryan.capehart@gmail.com>
2024-01-07 14:40:20 -0600

    missed a package

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,21 @@ const theme = createTheme();
 
 function App() {
   return (
-    <>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Paper
-          component="main"
-          sx={{
-            backgroundColor: (theme) => (theme.palette.mode === 'light' ? theme.palette.grey[100] : theme.palette.grey[900]),
-            flexGrow: 1,
-            height: '100vh',
-            overflow: 'auto'
-          }}
-        >
-          <h1>content</h1>
-          <SnowballCalculator />
-        </Paper>
-      </ThemeProvider>
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Paper
+        component="main"
+        sx={{
+          backgroundColor: (theme) => (theme.palette.mode === 'light' ? theme.palette.grey[100] : theme.palette.grey[900]),
+          flexGrow: 1,
+          height: '100vh',
+          overflow: 'auto'
+        }}
+      >
+        <h1>content</h1>
+        <SnowballCalculator />
+      </Paper>
+    </ThemeProvider>
   );
 }
 
--- src/components/snowballCalculator.tsx
+++ src/components/snowballCalculator.tsx
@@ -1,18 +1,18 @@
 import { FunctionComponent } from 'react';
-import useSnowballCalculatorHook from './useSnowballCalculatorHook';
-import { Container, Box } from '@mui/material';
+// import useSnowballCalculatorHook from './useSnowballCalculatorHook';
+// import { Container, Box } from '@mui/material';
 
 const SnowballCalculator: FunctionComponent = () => {
-  const vm = useSnowballCalculatorHook();
-
-  return (
-    <>
-      <p>test</p>
-      <Container>
-        <Box>{vm.title}</Box>
-      </Container>
-    </>
-  );
+  // const vm = useSnowballCalculatorHook();
+  return <div>test</div>;
+  // return (
+  //   <>
+  //     <p>test</p>
+  //     {/* <Container>
+  //       <Box>{vm.title}</Box>
+  //     </Container> */}
+  //   </>
+  // );
 };
 
 export default SnowballCalculator;
